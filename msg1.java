65e410d9f659eb2ec3d2a0653f0094adcfc0b356
Baba-ghub <fakruddin.ui26@gmail.com>
2026-07-22 12:28:32 +0530

    new changes commmited

--- msg1.java
+++ msg1.java
@@ -1,5 +1,6 @@
-
-public static void main(string[]) args())
-}
+public class Student{
+public static void main(string[]args){
 System.out printIn("Welcome to git session");
-System.out.printIn("Good")
+System.out.printIn("Good");
+  }
+  }
