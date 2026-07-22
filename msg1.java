ad78e639410b83419d687a0b4a80fa8ccaa5a90b
Baba-ghub <fakruddin.ui26@gmail.com>
2026-07-22 12:37:33 +0530

    CMT

--- msg1.java
+++ msg1.java
@@ -1,17 +1,14 @@
-public class Student {
+public class Packages {
 
     public static void main(String[] args) {
 
-        int rollno = 1596;
-        String sname = "KRISHNA";
+        int rollno = 9966;
+        String sname = "BABA";
         String course = "PYTHON";
-
-        System.out.println("LEARN HERE AND LEAD ANYWHERE!!!");
-        System.out.println("My Name is: " + sname);
+        
         System.out.println("My Course is: " + course);
         System.out.println("My Roll Number is: " + rollno);
-        System.out.println("Thanks for using my Application");
-        System.out.println("Thanks for joining today session Jenkins");
+       
 
     }
 
